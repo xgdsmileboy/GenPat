2158ab4652c354f2f92a97260a34fd06e0374226
xgdsmileboy <jiajun.jiang@pku.edu.cn>
2018-12-10 17:08:36 +0800

    add additional test case for relation parser

--- src/test/java/mfix/core/parse/PatternExtractionTest.java
+++ src/test/java/mfix/core/parse/PatternExtractionTest.java
@@ -13,6 +13,7 @@ import mfix.common.util.Pair;
 import mfix.core.TestCase;
 import mfix.core.parse.node.Node;
 import mfix.core.parse.relation.Pattern;
+import mfix.core.parse.relation.Relation;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.junit.Assert;
@@ -56,6 +57,43 @@ public class PatternExtractionTest extends TestCase {
         Assert.assertTrue(changedMethod.contains("fireTaskStarted"));
         Assert.assertTrue(changedMethod.contains("fireTaskFinished"));
         Assert.assertTrue(changedMethod.contains("fireMessageLoggedEvent"));
+    }
+
+    @Test
+    public void test_relation() {
+        String srcFile = testbase + Constant.SEP + "src_Project.java";
+        String tarFile = testbase + Constant.SEP + "tar_Project.java";
+
+        CompilationUnit srcUnit = JavaFile.genASTFromFileWithType(srcFile, null);
+        CompilationUnit tarUnit = JavaFile.genASTFromFileWithType(tarFile, null);
+        List<Pair<MethodDeclaration, MethodDeclaration>> matchMap = Matcher.match(srcUnit, tarUnit);
+        NodeParser nodeParser = NodeParser.getInstance();
+        for(Pair<MethodDeclaration, MethodDeclaration> pair : matchMap) {
+            if(pair.getFirst().getName().getIdentifier().equals("Project")) {
+                nodeParser.setCompilationUnit(srcFile, srcUnit);
+                Node srcNode = nodeParser.process(pair.getFirst());
+                nodeParser.setCompilationUnit(tarFile, tarUnit);
+                Node tarNode = nodeParser.process(pair.getSecond());
+                Pattern pattern = PatternExtraction.extract(srcNode, tarNode);
+                List<Relation> relations = pattern.getOldRelations();
+                // totally, there should be 10 relations
+                // 3 variable definitions
+                // 1 structure
+                // 2 method calls (method invocation and class instance creation)
+                // 2 assignments
+                // 2 kids (two statements in the method body)
+                Assert.assertTrue(relations.size() == 10);
+                // the first one should be the method structure
+                Assert.assertTrue(relations.get(0).getRelationKind() == Relation.RelationKind.STRUCTURE);
+
+                //the new relations should be the same since this method was not modified
+                relations = pattern.getNewRelations();
+                Assert.assertTrue(relations.size() == 10);
+                Assert.assertTrue(relations.get(0).getRelationKind() == Relation.RelationKind.STRUCTURE);
+                break;
+            }
+        }
+
     }
 
 }
