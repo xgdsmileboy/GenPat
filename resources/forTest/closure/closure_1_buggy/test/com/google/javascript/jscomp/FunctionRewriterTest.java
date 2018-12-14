9e08be82a5639e00e6c234f5bca43033adaa963c
Luyao Ren <375833274@qq.com>
2018-12-14 22:31:47 +0800

    rollback for mistake

--- resources/forTest/closure/closure_1_buggy/test/com/google/javascript/jscomp/FunctionRewriterTest.java
+++ resources/forTest/closure/closure_1_buggy/test/com/google/javascript/jscomp/FunctionRewriterTest.java
@@ -166,16 +166,16 @@ public class FunctionRewriterTest extends CompilerTestCase {
     checkCompilesToSame(      "/** @constructor */\n" +
         "WebInspector.Setting = function() {}\n" +
         "WebInspector.Setting.prototype = {\n" +
-        "    get name0(){return this._elementName;},\n" +
-        "    get name1(){return this._elementName;},\n" +
-        "    get name2(){return this._elementName;},\n" +
-        "    get name3(){return this._elementName;},\n" +
-        "    get name4(){return this._elementName;},\n" +
-        "    get name5(){return this._elementName;},\n" +
-        "    get name6(){return this._elementName;},\n" +
-        "    get name7(){return this._elementName;},\n" +
-        "    get name8(){return this._elementName;},\n" +
-        "    get name9(){return this._elementName;},\n" +
+        "    get name0(){return this._name;},\n" +
+        "    get name1(){return this._name;},\n" +
+        "    get name2(){return this._name;},\n" +
+        "    get name3(){return this._name;},\n" +
+        "    get name4(){return this._name;},\n" +
+        "    get name5(){return this._name;},\n" +
+        "    get name6(){return this._name;},\n" +
+        "    get name7(){return this._name;},\n" +
+        "    get name8(){return this._name;},\n" +
+        "    get name9(){return this._name;},\n" +
         "}", 1);
   }
 
