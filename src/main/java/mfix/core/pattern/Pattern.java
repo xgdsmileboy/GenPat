cef189c55e7b3965c269fd559d3921dba5abd100
xgdsmileboy <jiajun.jiang@pku.edu.cn>
2019-01-08 19:23:21 +0800

    minor

--- src/main/java/mfix/core/pattern/Pattern.java
+++ src/main/java/mfix/core/pattern/Pattern.java
@@ -16,7 +16,13 @@ import mfix.core.pattern.solver.Z3Solver;
 import mfix.core.stats.element.ElementCounter;
 
 import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author: Jiajun
