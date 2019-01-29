090ddd2a9af09117cfdd9e2467cfd31ac7012034
xgdsmileboy <xgd_smileboy@163.com>
2019-01-29 15:17:22 +0800

    upd main func

--- src/main/java/mfix/Main.java
+++ src/main/java/mfix/Main.java
@@ -8,17 +8,23 @@ package mfix;
 
 import mfix.common.util.JavaFile;
 import mfix.common.util.Pair;
+import mfix.common.util.Utils;
 import mfix.core.node.MatchInstance;
+import mfix.core.node.NodeUtils;
 import mfix.core.node.PatternExtractor;
 import mfix.core.node.ast.MethDecl;
 import mfix.core.node.ast.Node;
+import mfix.core.node.ast.NodeVisitor;
 import mfix.core.node.ast.expr.MethodInv;
 import mfix.core.node.match.Matcher;
 import mfix.core.node.modify.Modification;
 import mfix.core.node.parser.NodeParser;
+
+import org.eclipse.core.internal.resources.Folder;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.internal.eval.VariablesEvaluator;
 
 import java.io.*;
 import java.util.*;
@@ -265,8 +271,8 @@ public class Main {
                 }
                 
                 
-                if (cnt >= 100000) {
-                	break;
+                if (cnt >= 5000000) {
+                	break;	
                 }
                 
                 
@@ -287,15 +293,15 @@ public class Main {
     }
 
 
-    public static void tryMatchAndFix(Node buggy, Node pattern, String patternFile) throws Exception {
-		Set<MatchInstance> set = Matcher.tryMatch(buggy, pattern);
+    public static void tryMatchAndFix(Node buggy, Node pattern, Set<String> buggyMethodVar, String buggyFile, String patternFile) throws Exception {
+		Set<MatchInstance> fixPositions = Matcher.tryMatch(buggy, pattern);
 		
 		String origin = buggy.toString();
 		
-        for (MatchInstance matchInstance : set) {
+        for (MatchInstance matchInstance : fixPositions) {
             matchInstance.apply();
             
-            StringBuffer fixedProg = buggy.adaptModifications();
+            StringBuffer fixedProg = buggy.adaptModifications(buggyMethodVar);
             
             if (fixedProg != null) {
                 String fixed = fixedProg.toString().replaceAll(" ", "");
@@ -307,6 +313,10 @@ public class Main {
                 	fixedRet.add(fixed);
                 	
                 	System.out.println(patternFile);
+
+                	System.out.println("------------ Origin ---------------");
+                	System.out.println(origin);
+                	
                 	System.out.println("------------ Solution ---------------");
                 	System.out.println(fixedProg);
                 	
@@ -317,6 +327,7 @@ public class Main {
                 }
             }
             
+            matchInstance.reset();
             // System.out.println(matchInstance.getNodeMap());   
         }
     }
@@ -334,9 +345,16 @@ public class Main {
 
         NodeParser parser = NodeParser.getInstance();
         parser.setCompilationUnit(buggyFile, unit);
-
+        	
+        
+        Map<Integer, Set<String>> buggyFileVarMap = NodeUtils.getUsableVarTypes(buggyFile);
+		
+		
+		
         for(MethodDeclaration m : methods) {
             Node node = parser.process(m);
+            
+            Set<String> buggyMethodVar = buggyFileVarMap.getOrDefault(node.getStartLine(), new HashSet<String>());
 
             Set<MethodInv> ContainMethodInvs = node.getUniversalAPIs(new HashSet<MethodInv>(), false);
             
@@ -361,13 +379,16 @@ public class Main {
                 System.out.println(" Size of patternList : " + patternFileList.size());
                
                 
-                /*
-                if (!(MethodName.equals("dismiss") && MethodArgsNum == 0)) {
+                
+//                if (!(MethodName.equals("dismiss") && MethodArgsNum == 0)) {
+//                	continue;
+//                }
+                
+                if (!(MethodName.equals("getAsString") && MethodArgsNum == 0)) {
                 	continue;
                 }
-                */
                 
-            
+          
                 /*
                 if (!(MethodName.equals("isAJavaToken"))) {
                 	continue;
@@ -384,7 +405,87 @@ public class Main {
 	                try {
 	                	// System.out.println(patternFile);
 	                	
-	                	tryMatchAndFix(node, loadPatternFromFile(patternFile), patternFile);
+//	                	if (!(patternFile.equals("/home/lee/Xia/GitHubData/MissSome/2015/V26/7040/pattern-ver4-serial/app.src.main.java.com.sqbnet.expressassistant.registrationActivity.java-run.pattern"))) {
+//	                		continue;
+//	                	}
+//	                	
+//	            		Set<Node> patterns = PatternExtractor.extractPattern("/home/lee/Xia/GitHubData/MissSome/2015/V26/7040/buggy-version/app.src.main.java.com.sqbnet.expressassistant.registrationActivity.java",
+//	                			"/home/lee/Xia/GitHubData/MissSome/2015/V26/7040/fixed-version/app.src.main.java.com.sqbnet.expressassistant.registrationActivity.java"
+//	                			);
+//	            		
+	                	
+	                	
+
+	            		int ind = patternFile.indexOf("pattern-ver4-serial");
+	            		int indLen = "pattern-ver4-serial".length();
+	            		String filePath = patternFile.substring(0, ind - 1);
+	            		String fileAndMethod = patternFile.substring(ind + indLen + 1);
+
+	            		int dashIndex = fileAndMethod.lastIndexOf("-");
+	            		String file = fileAndMethod.substring(0, dashIndex);
+	            		String method = fileAndMethod.substring(dashIndex + 1);
+	            		
+	            		String versionFolder = "ver6";
+	            		File versionAbsFolder = new File(filePath + "/" + versionFolder);
+	                    if (!versionAbsFolder.exists()) {
+	                    	versionAbsFolder.mkdirs();
+	                    }
+
+	            		String patternSerializePath = filePath + "/" + versionFolder + "/" + fileAndMethod;
+	            		
+	            		
+//	            		String mypattern = "/home/lee/Xia/GitHubData/MissSome/2012-2014/V64/6552/ver6/CacheCleaner.src.main.java.com.frozendevs.cache.cleaner.activity.CleanerActivity.java-onScanStarted.pattern";
+//	            		
+//	            		if (!(patternSerializePath.equals(mypattern))) {
+//	            			continue;
+//	            		}
+	            		
+	            		
+	            		if (!(new File(patternSerializePath)).exists()) {
+		            		Set<Node> patternCandidates = PatternExtractor.extractPattern(filePath + "/buggy-version/" + file,
+		            				filePath + "/fixed-version/" + file);
+		            		
+		            		for (Node fixPattern : patternCandidates) {
+		            			MethDecl methDecl = (MethDecl) fixPattern;
+		            			String patternFuncName = methDecl.getName().getName();
+
+		            			String savePatternPath = filePath + "/" + versionFolder + "/" + file + "-" + patternFuncName + ".pattern";
+		            			System.out.println("save pattern =" + savePatternPath);
+		            			
+		            			Utils.serialize(fixPattern, savePatternPath);
+		            		}
+		   
+	            		} else {
+	            			// System.out.println("skip for " + patternSerializePath);
+	            		}
+	            		
+	            		
+	            		
+	            		
+	            		
+	            		System.out.println("current:" + patternSerializePath);
+	            		
+	            		
+	            		Node fixPattern = (Node)Utils.deserialize(patternSerializePath);
+	            		
+	            		tryMatchAndFix(node, fixPattern, buggyMethodVar, buggyFile, patternSerializePath);
+	            		
+	            		/*
+	            		System.out.println(filePath);
+	            		System.out.println(file);
+	            		System.out.println(method);
+	            		*/
+	            		// System.out.println(patternFile);
+	            		
+	            		
+	            																																				
+	            		
+	                	
+	                	
+	                	
+	                	// System.out.println("-------");
+	                	// tryMatchAndFix(node, loadPatternFromFile(patternFile), patternFile);
+	                	
 	                } catch (Exception e) {
 	                    e.printStackTrace();
 	                }
@@ -422,14 +523,31 @@ public class Main {
     	*/
     	
     	
-    	System.out.println("Hello!");
+    	// Node p = loadPatternFromFile("/home/lee/Xia/GitHubData/MissSome/2015/V26/7040/pattern-ver4-serial/app.src.main.java.com.sqbnet.expressassistant.registrationActivity.java-run.pattern");
     	
+		
+    	/*
+    	Node p = loadPatternFromFile("/home/jack/Desktop/0.pattern");
+    	    	
+    	p.accept(new NodeVisitor(){
+    		public boolean visit(MethodInv method) {
+    			System.out.println(method.getName().getName());
+    			System.out.println(method.isAbstract());
+    			System.out.println("----");
+    			
+    			return true;
+    		}
+    	});
+    	*/
+    	
+		
     	loadAPI();
     	
     	String buggyFilePath = "/home/jack/code/workspace/eclipse/MineFix/resources/forTest/buggy_SimpleSecureBrowser.java";
     	// String buggyFilePath = "/home/jack/Desktop/rly/cases/5/base-all.java";
     	
     	tryFix(buggyFilePath);
+    	
     	
     }
 }
