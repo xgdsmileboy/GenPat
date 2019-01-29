1c51da2db46ebf7fc928f3210b1308486ef8f6e0
xgdsmileboy <xgd_smileboy@163.com>
2019-01-29 15:52:23 +0800

    upd main func only for detection

--- src/main/java/mfix/Main.java
+++ src/main/java/mfix/Main.java
@@ -32,204 +32,14 @@ import java.util.concurrent.*;
 
 
 public class Main {
-
-    static void savePatternToFile(String outputFile, Node obj){
-        try {
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(outputFile));
-            oos.writeObject(obj);
-
-            System.out.println("save for" + outputFile);
-
-            oos.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    static Node loadPatternFromFile(String inputFile) {
-        try {
-            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(inputFile));
-            Node node = (Node)ois.readObject();
-            ois.close();
-            return node;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-        	e.printStackTrace();
-        }
-        return null;
-    }
-
-    static void printPattern(Set<Node> pattern) {
-        System.out.println("-------start---------");
-        for (Node node : pattern) {
-            System.out.println(node.getAllModifications(new HashSet<Modification>()));
-        }
-
-        System.out.println("--------------------");
-        System.out.println("-------end----------");
-    }
-
-
-    static void timeoutMethod(int timeout, String srcFile, String tarFile, String savePatternFolder) {
-        FutureTask<Boolean> futureTask = new FutureTask<>(new Callable<Boolean>() {
-            @Override
-            public Boolean call() throws Exception {
-                return extract(srcFile, tarFile, savePatternFolder);
-            }
-        });
-
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-
-        executorService.execute(futureTask);
-
-        try {
-            boolean result = futureTask.get(timeout, TimeUnit.SECONDS);
-        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-            e.printStackTrace();
-            System.out.println("Timeout or other error for" + srcFile);
-            futureTask.cancel(true);
-        }
-
-        executorService.shutdownNow();
-    }
-
-    static boolean extract(String srcFile, String tarFile, String savePatternFilePrefix) {
-    	
-		long start1 = System.currentTimeMillis();
-
-		Set<Node> patterns = PatternExtractor.extractPattern(srcFile, tarFile);
-		
-		for (Node pattern : patterns) {
-			MethDecl methDecl = (MethDecl) pattern;
-			String patternFuncName = methDecl.getName().getName();
-			
-			String savePath = savePatternFilePrefix + '-' + patternFuncName + ".pattern";
-			
-			savePatternToFile(savePath, pattern);
-			
-			Set<MethodInv> methods = pattern.getUniversalAPIs(new HashSet<MethodInv>(), true);
-			for (MethodInv method : methods) {
-				String methodName = method.getName().getName();
-				Integer methodExprSize = method.getArguments().getExpr().size();
-
-				JavaFile.writeStringToFile(System.getProperty("user.dir") + "/API_Mapping.txt",
-						methodName + " " + methodExprSize + " " + savePath + "\n", true);
-				
-			}
-			
-			// Node newp = loadPatternFromFile(saveFile);
-
-	        // System.out.println("-----before save-------");
-	        // printPattern(patterns);
-	        
-	        // System.out.println("-----after save-------");
-	        // printPattern(newp);
-		}
-		
-		JavaFile.writeStringToFile(System.getProperty("user.dir") + "/time.log",
-				srcFile + "[" + (System.currentTimeMillis() - start1) / 1000 + " s]\n", true);
-
-        return true;
-    }
-
-    static void runFolder(String folderPath) throws Exception {
-        File buggyFolder = new File(folderPath + "/buggy-version");
-        File fixedFolder = new File(folderPath + "/fixed-version");
-
-        if ((!buggyFolder.exists()) || (!fixedFolder.exists())) {
-            throw new Exception("Files not exist!");
-        }
-
-        List<File> buggyFiles = JavaFile.ergodic(buggyFolder, new LinkedList<>());
-        List<File> fixedFiles = JavaFile.ergodic(fixedFolder, new LinkedList<>());
-        HashMap<String, File> buggyFilesMap = new HashMap<String, File>();
-
-        for (File file : buggyFiles) {
-            buggyFilesMap.put(file.getName(), file);
-        }
-
-        for (File fixedFile : fixedFiles) {
-            File buggyFile = buggyFilesMap.getOrDefault(fixedFile.getName(), null);
-
-            File saveFolder = new File(folderPath + "/pattern-ver4-serial");
-            if (!saveFolder.exists()) {
-                saveFolder.mkdirs();
-            } else {
-            	continue;
-            }
-
-            String savePatternFolder = saveFolder.getAbsolutePath() + "/" + fixedFile.getName();
-
-            if (buggyFile != null) {
-                try {
-                    timeoutMethod(60 * 2, buggyFile.getAbsolutePath(), fixedFile.getAbsolutePath(), savePatternFolder);
-                } catch (Exception e) {
-                    // TODO: handle exception
-                }
-
-                // extract(buggyFile.getAbsolutePath(), fixedFile.getAbsolutePath(), saveFile);
-            }
-        }
-
-        //System.out.println(buggyFiles.toString());
-        //System.out.println(fixedFiles.toString());
-    }
-
-    static void work(String fileListFilePath) {
-        FileInputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream(fileListFilePath);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-        Integer cnt = 0;
-        String filePath = null;
-        while(true)
-        {
-            try {
-                filePath = bufferedReader.readLine();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            if (filePath == null) {
-                break;
-            }
-
-            cnt += 1;
-            System.out.println(cnt.toString() + ":" + filePath);
-            try {
-                runFolder(filePath);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        try {
-        	bufferedReader.close();
-            inputStream.close();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-    
     // static String APIMappingFile = "/home/jack/Desktop/rly/api-test.txt";
     static String APIMappingFile = "/home/jack/Desktop/rly/API_Mapping.txt";
     // TODO(rly): is hashset could avoid repeat??
     static Map<Pair<String, Integer>, Set<String>> method2PatternFiles;
     
     static Set<String> fixedRet = new HashSet<String>();
+    
+    static String versionFolder = "ver6";
     
     static void loadAPI() {
         System.out.println("Start Load API Mappings!");
@@ -270,10 +80,9 @@ public class Main {
                 	System.out.println(cnt);
                 }
                 
-                
-                if (cnt >= 5000000) {
-                	break;	
-                }
+//                if (cnt >= 5000000) {
+//                	break;	
+//                }
                 
                 
             } catch (Exception e) {
@@ -320,8 +129,8 @@ public class Main {
                 	System.out.println("------------ Solution ---------------");
                 	System.out.println(fixedProg);
                 	
-                	JavaFile.writeStringToFile("/home/jack/Desktop/rly/fix_result.txt",
-                			fixedProg + "\n---------------\n", true);
+                	JavaFile.writeStringToFile("/home/jack/Desktop/rly/new_fix_result.txt",
+                			buggyFile + ":\n" + fixedProg + "\n---------------\n", true);
                 	
                 	System.out.println("------------ End ---------------");
                 }
@@ -345,13 +154,13 @@ public class Main {
 
         NodeParser parser = NodeParser.getInstance();
         parser.setCompilationUnit(buggyFile, unit);
-        	
         
         Map<Integer, Set<String>> buggyFileVarMap = NodeUtils.getUsableVarTypes(buggyFile);
 		
-		
-		
         for(MethodDeclaration m : methods) {
+        	
+        	System.out.println("Method: " + m.getName().getFullyQualifiedName());
+        	
             Node node = parser.process(m);
             
             Set<String> buggyMethodVar = buggyFileVarMap.getOrDefault(node.getStartLine(), new HashSet<String>());
@@ -379,23 +188,15 @@ public class Main {
                 System.out.println(" Size of patternList : " + patternFileList.size());
                
                 
+                if (!(MethodName.equals("dismiss") && MethodArgsNum == 0)) {
+                	continue;
+                }
                 
-//                if (!(MethodName.equals("dismiss") && MethodArgsNum == 0)) {
+//                if (!(MethodName.equals("getAsString") && MethodArgsNum == 0)) {
 //                	continue;
 //                }
                 
-                if (!(MethodName.equals("getAsString") && MethodArgsNum == 0)) {
-                	continue;
-                }
-                
-          
-                /*
-                if (!(MethodName.equals("isAJavaToken"))) {
-                	continue;
-                }
-                */
-                
-                
+             
                 System.out.println("Start matching!");
                 
             	// System.out.println("------------ Original ---------------");
@@ -403,19 +204,6 @@ public class Main {
             	
                 for (String patternFile : patternFileList) {
 	                try {
-	                	// System.out.println(patternFile);
-	                	
-//	                	if (!(patternFile.equals("/home/lee/Xia/GitHubData/MissSome/2015/V26/7040/pattern-ver4-serial/app.src.main.java.com.sqbnet.expressassistant.registrationActivity.java-run.pattern"))) {
-//	                		continue;
-//	                	}
-//	                	
-//	            		Set<Node> patterns = PatternExtractor.extractPattern("/home/lee/Xia/GitHubData/MissSome/2015/V26/7040/buggy-version/app.src.main.java.com.sqbnet.expressassistant.registrationActivity.java",
-//	                			"/home/lee/Xia/GitHubData/MissSome/2015/V26/7040/fixed-version/app.src.main.java.com.sqbnet.expressassistant.registrationActivity.java"
-//	                			);
-//	            		
-	                	
-	                	
-
 	            		int ind = patternFile.indexOf("pattern-ver4-serial");
 	            		int indLen = "pattern-ver4-serial".length();
 	            		String filePath = patternFile.substring(0, ind - 1);
@@ -425,7 +213,7 @@ public class Main {
 	            		String file = fileAndMethod.substring(0, dashIndex);
 	            		String method = fileAndMethod.substring(dashIndex + 1);
 	            		
-	            		String versionFolder = "ver6";
+	            		
 	            		File versionAbsFolder = new File(filePath + "/" + versionFolder);
 	                    if (!versionAbsFolder.exists()) {
 	                    	versionAbsFolder.mkdirs();
@@ -442,7 +230,8 @@ public class Main {
 	            		
 	            		
 	            		if (!(new File(patternSerializePath)).exists()) {
-		            		Set<Node> patternCandidates = PatternExtractor.extractPattern(filePath + "/buggy-version/" + file,
+		            		Set<Node> patternCandidates = PatternExtractor.extractPattern(
+		            				filePath + "/buggy-version/" + file,
 		            				filePath + "/fixed-version/" + file);
 		            		
 		            		for (Node fixPattern : patternCandidates) {
@@ -450,7 +239,7 @@ public class Main {
 		            			String patternFuncName = methDecl.getName().getName();
 
 		            			String savePatternPath = filePath + "/" + versionFolder + "/" + file + "-" + patternFuncName + ".pattern";
-		            			System.out.println("save pattern =" + savePatternPath);
+		            			System.out.println("save pattern: " + savePatternPath);
 		            			
 		            			Utils.serialize(fixPattern, savePatternPath);
 		            		}
@@ -459,12 +248,8 @@ public class Main {
 	            			// System.out.println("skip for " + patternSerializePath);
 	            		}
 	            		
-	            		
-	            		
-	            		
-	            		
-	            		System.out.println("current:" + patternSerializePath);
-	            		
+
+	            		// System.out.println("current:" + patternSerializePath);
 	            		
 	            		Node fixPattern = (Node)Utils.deserialize(patternSerializePath);
 	            		
@@ -474,18 +259,13 @@ public class Main {
 	            		System.out.println(filePath);
 	            		System.out.println(file);
 	            		System.out.println(method);
-	            		*/
+	            		
 	            		// System.out.println(patternFile);
 	            		
-	            		
-	            																																				
-	            		
-	                	
-	                	
 	                	
 	                	// System.out.println("-------");
-	                	// tryMatchAndFix(node, loadPatternFromFile(patternFile), patternFile);
-	                	
+	                	*/
+	                
 	                } catch (Exception e) {
 	                    e.printStackTrace();
 	                }
