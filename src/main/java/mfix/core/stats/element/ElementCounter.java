0aa7d2e5ade5ac2ff2a1098f54e1cb98d5bbb521
Luyao Ren <fancycoder0@gmail.com>
2019-02-03 10:11:04 +0800

    upd counter

--- src/main/java/mfix/core/stats/element/ElementCounter.java
+++ src/main/java/mfix/core/stats/element/ElementCounter.java
@@ -19,6 +19,11 @@ public class ElementCounter {
     private static HashMap<Pair<String, Integer>, Integer> cacheMapForAPI = null;
     private static HashMap<Pair<Pair<String, Integer>, String>, Integer> cacheMapForAPIWithType = null;
 
+    private static Integer cacheTotalNumberforAPI = null;
+    private static Integer cacheTotalNumberforAPIWithType = null;
+
+    static final String WARNNING_FOR_QUERY_DB = "[WARNING] Query on Database.";
+
     public void open() {
         _connector = new DatabaseConnector();
         _connector.open();
@@ -44,9 +49,13 @@ public class ElementCounter {
                 throw new ElementException(element.DBKEY_ARGS_NUMBER);
             }
             countNumber = cacheMapForAPI.getOrDefault(new Pair<String, Integer>(methodElement._elementName, methodElement._argsNumber), 0);
+
             if (queryType.getWithPercent()) {
-                LevelLogger.error("[ERROR] Query on Database.");
-                allNumber = _connector.query(element.toQueryRowWithoutLimit(queryType));
+                if (cacheTotalNumberforAPI == null) {
+                    LevelLogger.warn(WARNNING_FOR_QUERY_DB);
+                    cacheTotalNumberforAPI = _connector.query(element.toQueryRowWithoutLimit(queryType));
+                }
+                allNumber = cacheTotalNumberforAPI;
             }
         } else if ((cacheMapForAPIWithType != null) && (element instanceof MethodElement) && (queryType.getWithType()) && (queryType._countType == ElementQueryType.CountType.COUNT_FILES)) {
             MethodElement methodElement = (MethodElement)element;
@@ -60,14 +69,17 @@ public class ElementCounter {
             if (methodElement._objType == null) {
                 throw new ElementException(element.DBKEY_OBJ_TYPE);
             }
-
             countNumber = cacheMapForAPIWithType.getOrDefault(new Pair<>(new Pair<>(methodElement._elementName, methodElement._argsNumber), methodElement._objType), 0);
+
             if (queryType.getWithPercent()) {
-                LevelLogger.error("[ERROR] Query on Database.");
-                allNumber = _connector.query(element.toQueryRowWithoutLimit(queryType));;
+                if (cacheTotalNumberforAPIWithType == null) {
+                    LevelLogger.warn(WARNNING_FOR_QUERY_DB);
+                    cacheTotalNumberforAPIWithType = _connector.query(element.toQueryRowWithoutLimit(queryType));
+                }
+                allNumber = cacheTotalNumberforAPIWithType;
             }
         } else {
-            LevelLogger.error("[ERROR] Query on Database.");
+            LevelLogger.warn(WARNNING_FOR_QUERY_DB);
             countNumber = _connector.query(element.toQueryRow(queryType));
             allNumber = _connector.query(element.toQueryRowWithoutLimit(queryType));
         }
