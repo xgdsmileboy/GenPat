c670f3a5d187097cc1a2e0b430ed112bdea8230a
Luyao Ren <375833274@qq.com>
2019-02-13 21:44:41 +0800

    updater TokenGetter

--- src/main/java/mfix/core/stats/TokenGetter.java
+++ src/main/java/mfix/core/stats/TokenGetter.java
@@ -3,21 +3,19 @@ package mfix.core.stats;
 import mfix.common.util.Utils;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class TokenGetter {
     static private HashMap<String, Integer> counter;
 
     static final String SAVE_FILE = "/home/renly/tokenSerial-tmp/data";
 
-    public void readFromBatch() {
+    static public void readFromBatch() {
         HashMap<String, Integer> tmpCounter;
 
         counter = new HashMap<String, Integer>();
 
-        for (Integer batchNumber = 1; true; batchNumber++) {
+        for (Integer batchNumber = 1; batchNumber <= 3; batchNumber++) {
             String path = SAVE_FILE + "-batch" + batchNumber.toString();
             if (!(new File(path)).exists()) {
                 break;
@@ -26,12 +24,40 @@ public class TokenGetter {
             try {
                 tmpCounter = (HashMap<String, Integer>) Utils.deserialize(path);
                 for (Map.Entry<String, Integer> entry : tmpCounter.entrySet()) {
-                    System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-                    // counter.put(entry.getKey(), counter.getOrDefault(entry.getKey(), 0) + entry.getValue());
+                    // System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+                    counter.put(entry.getKey(), counter.getOrDefault(entry.getKey(), 0) + entry.getValue());
+
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+
+        /*
+        List<Map.Entry<String, Integer>> list = new ArrayList<>();
+
+        for(Map.Entry<String, Integer> entry : counter.entrySet()){
+            list.add(entry);
+        }
+
+        list.sort(new Comparator<Map.Entry<String, Integer>>(){
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue()-o1.getValue();
+            }
+        });
+        */
+
+        /*
+        int cnt = 0;
+        for(Map.Entry<String, Integer> entry: list){
+            System.out.println(entry);
+
+            cnt += 1;
+            if (cnt >= 50) {
+                break;
+            }
+        }
+        */
     }
 }
