9c4021c57a5c2958a095fba2be4a16bf135b80fe
Luyao Ren <fancycoder0@gmail.com>
2019-02-18 12:59:32 +0800

    add TokenGetter

---
+++ src/main/java/mfix/core/stats/TokenGetter.java
@@ -0,0 +1,78 @@
+package mfix.core.stats;
+
+import mfix.common.util.Utils;
+
+import java.io.*;
+import java.util.*;
+
+public class TokenGetter {
+    static private HashMap<String, Integer> counter;
+
+    static final String SAVE_FILE = "/home/renly/tokenSerial/data";
+
+    static final String TEMP_SERIALIZE_FILE = "/home/renly/tokenSerial/map_for_all";
+
+    static final String IMPORT_FILE = "/home/renly/tokenSerial/data/all.txt";
+
+    static public void readFromBatch() {
+        if (!(new File(TEMP_SERIALIZE_FILE)).exists()) {
+            try {
+                counter = (HashMap<String, Integer>) Utils.deserialize(TEMP_SERIALIZE_FILE);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            HashMap<String, Integer> tmpCounter;
+            counter = new HashMap<String, Integer>();
+            for (Integer batchNumber = 1; true; batchNumber++) {
+                String path = SAVE_FILE + "-batch" + batchNumber.toString();
+                if (!(new File(path)).exists()) {
+                    break;
+                }
+
+                try {
+                    tmpCounter = (HashMap<String, Integer>) Utils.deserialize(path);
+                    for (Map.Entry<String, Integer> entry : tmpCounter.entrySet()) {
+                        int value = entry.getValue();
+                        counter.put(entry.getKey(), counter.getOrDefault(entry.getKey(), 0) + value);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    static public void importAsTextFile() {
+        List<Map.Entry<String, Integer>> list = new ArrayList<>();
+        for(Map.Entry<String, Integer> entry : counter.entrySet()){
+            list.add(entry);
+        }
+
+        list.sort(new Comparator<Map.Entry<String, Integer>>(){
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue()-o1.getValue();
+            }
+        });
+
+        try
+        {
+            FileOutputStream fos = new FileOutputStream(IMPORT_FILE);
+            Writer out = new OutputStreamWriter(fos, "UTF-8");
+            int cnt = 0;
+            for(Map.Entry<String, Integer> entry: list) {
+                out.write(entry.getKey());
+                out.write(entry.getValue());
+                break;
+            }
+            out.close();
+            fos.close();
+        } catch (IOException e)
+        {
+            System.out.println("Write Error!");
+            e.printStackTrace();
+        }
+    }
+}
