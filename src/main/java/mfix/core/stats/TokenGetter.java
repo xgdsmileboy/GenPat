e3b8829135a74d765a275012817210513ec918a6
Luyao Ren <fancycoder0@gmail.com>
2019-02-18 13:12:13 +0800

    add TokenGetter

--- src/main/java/mfix/core/stats/TokenGetter.java
+++ src/main/java/mfix/core/stats/TokenGetter.java
@@ -9,19 +9,24 @@ public class TokenGetter {
     static private HashMap<String, Integer> counter;
 
     static final String SAVE_FILE = "/home/renly/tokenSerial/data";
+    //static final String SAVE_FILE = "/Users/luyaoren/workplace/tokenSerial/data";
 
     static final String TEMP_SERIALIZE_FILE = "/home/renly/tokenSerial/map_for_all";
+    //static final String TEMP_SERIALIZE_FILE = "/Users/luyaoren/workplace/tokenSerial/map_for_all";
 
     static final String IMPORT_FILE = "/home/renly/tokenSerial/data/all.txt";
+    //static final String IMPORT_FILE = "/Users/luyaoren/workplace/tokenSerial/all.txt";
 
     static public void readFromBatch() {
-        if (!(new File(TEMP_SERIALIZE_FILE)).exists()) {
+        if ((new File(TEMP_SERIALIZE_FILE)).exists()) {
+            System.out.println("Already exist!");
             try {
                 counter = (HashMap<String, Integer>) Utils.deserialize(TEMP_SERIALIZE_FILE);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         } else {
+            System.out.println("First load!");
             HashMap<String, Integer> tmpCounter;
             counter = new HashMap<String, Integer>();
             for (Integer batchNumber = 1; true; batchNumber++) {
@@ -40,11 +45,23 @@ public class TokenGetter {
                     e.printStackTrace();
                 }
             }
+
+
+            try {
+                Utils.serialize(counter, TEMP_SERIALIZE_FILE);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
 
-    static public void importAsTextFile() {
+    static public void importAsTextFile() throws Exception{
+        if (counter == null) {
+            throw new Exception("Please first load!");
+        }
+
         List<Map.Entry<String, Integer>> list = new ArrayList<>();
         for(Map.Entry<String, Integer> entry : counter.entrySet()){
             list.add(entry);
@@ -65,8 +82,8 @@ public class TokenGetter {
             for(Map.Entry<String, Integer> entry: list) {
                 out.write(entry.getKey());
                 out.write(entry.getValue());
-                break;
             }
+            System.out.println("Finish!");
             out.close();
             fos.close();
         } catch (IOException e)
