ce0e8e57d6f4401fe07cb0c17d8e5e645927b79b
xgdsmileboy <jiajun.jiang@pku.edu.cn>
2019-03-25 22:42:24 -0500

    add test case for MuBench

---
+++ resources/forTest/pattern4Mubench/alibaba-druid/2/buggy.java
@@ -0,0 +1,13 @@
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+
+class HandleInvalidKey {
+    void pattern(PrivateKey privateKey) throws NoSuchAlgorithmException, NoSuchPaddingException {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+    }
+}
