a1599a255aeefe49dbbcb7135e2c29477bb0bdad
xgdsmileboy <jiajun.jiang@pku.edu.cn>
2019-04-24 12:12:03 -0500

    add suport for AssertStatement

--- src/main/java/mfix/core/node/ast/stmt/AssertStmt.java
+++ src/main/java/mfix/core/node/ast/stmt/AssertStmt.java
@@ -6,11 +6,15 @@
  */
 package mfix.core.node.ast.stmt;
 
+import mfix.common.util.LevelLogger;
 import mfix.core.node.NodeUtils;
 import mfix.core.node.ast.MatchLevel;
 import mfix.core.node.ast.Node;
 import mfix.core.node.ast.VarScope;
+import mfix.core.node.ast.expr.Expr;
 import mfix.core.node.match.metric.FVector;
+import mfix.core.node.modify.Modification;
+import mfix.core.node.modify.Update;
 import mfix.core.pattern.cluster.NameMapping;
 import mfix.core.pattern.cluster.VIndex;
 import org.eclipse.jdt.core.dom.ASTNode;
@@ -28,7 +32,8 @@ import java.util.Set;
 public class AssertStmt extends Stmt {
 
 	private static final long serialVersionUID = 8494694375316529776L;
-	private String _codeStr;
+	private Expr _expresion;
+	private Expr _message;
 	// TODO: assert statement should be added
 	/**
 	 * AssertStatement:
@@ -42,12 +47,34 @@ public class AssertStmt extends Stmt {
 		super(fileName, startLine, endLine, node, parent);
 		_nodeType = TYPE.ASSERT;
 		_fIndex = VIndex.STMT_ASSERT;
-		_codeStr = node.toString();
+	}
+
+	public void setExpression(Expr expression) {
+		_expresion = expression;
+	}
+
+	public Expr getExpression() {
+		return _expresion;
+	}
+
+	public void setMessage(Expr message) {
+		_message = message;
+	}
+
+	public Expr getMessage() {
+		return _message;
 	}
 
 	@Override
 	public StringBuffer toSrcString() {
-		return new StringBuffer(_codeStr);
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("assert ")
+				.append(_expresion.toSrcString());
+		if (_message != null) {
+			buffer.append(":").append(_message.toSrcString());
+		}
+		buffer.append(";");
+		return buffer;
 	}
 
 	@Override
@@ -65,7 +92,11 @@ public class AssertStmt extends Stmt {
 	@Override
 	protected void tokenize() {
 		_tokens = new LinkedList<>();
-		_tokens.add(_codeStr);
+		_tokens.add("assert");
+		_tokens.addAll(_expresion.tokens());
+		if (_message != null) {
+			_tokens.addAll(_message.tokens());
+		}
 	}
 
 	@Override
@@ -76,14 +107,26 @@ public class AssertStmt extends Stmt {
 	@Override
 	public boolean compare(Node other) {
 		if (other instanceof AssertStmt) {
-			return _codeStr.equals(((AssertStmt) other)._codeStr);
+			AssertStmt assertStmt = (AssertStmt) other;
+			if (_expresion.compare(assertStmt.getExpression())) {
+				if (_message == null) {
+					return assertStmt.getMessage() == null;
+				} else {
+					return _message.compare(assertStmt.getMessage());
+				}
+			}
 		}
 		return false;
 	}
 
 	@Override
 	public List<Node> getAllChildren() {
-		return new ArrayList<>(0);
+		List<Node> nodes = new ArrayList<>(2);
+		nodes.add(_expresion);
+		if (_message != null) {
+			nodes.add(_message);
+		}
+		return nodes;
 	}
 
 	@Override
@@ -93,27 +136,69 @@ public class AssertStmt extends Stmt {
 
 		_completeFVector = new FVector();
 		_completeFVector.inc(FVector.KEY_ASSERT);
+		_completeFVector.combineFeature(_expresion.getFeatureVector());
+		if (_message != null) {
+			_completeFVector.combineFeature(_message.getFeatureVector());
+		}
 	}
 
 	@Override
 	public boolean postAccurateMatch(Node node) {
-		if (getBindingNode() == node) return true;
-		if (getBindingNode() == null && canBinding(node)) {
-			setBindingNode(node);
+		boolean match = false;
+		AssertStmt assertStmt = null;
+		if (getBindingNode() != null && (getBindingNode() == node || !compare(node))) {
+			assertStmt = (AssertStmt) getBindingNode();
+			match = (assertStmt == node);
+		} else if (canBinding(node)) {
+			assertStmt = (AssertStmt) node;
+			setBindingNode(assertStmt);
+			match = true;
+		}
+
+		if (assertStmt != null) {
+			_expresion.postAccurateMatch(assertStmt.getExpression());
+			if (_message != null && assertStmt.getMessage() != null) {
+				_message.postAccurateMatch(assertStmt.getMessage());
+			}
+		} else {
+			continueTopDownMatchNull();
+		}
+
+		return match;
+	}
+
+	@Override
+	public boolean genModifications() {
+		if (super.genModifications()) {
+			AssertStmt assertStmt = (AssertStmt) getBindingNode();
+			if (_expresion.getBindingNode() != assertStmt.getExpression()) {
+				Update update = new Update(this, _expresion, assertStmt.getExpression());
+				_modifications.add(update);
+			} else {
+				_expresion.genModifications();
+			}
+			if (_message == null) {
+				if (assertStmt.getMessage() != null) {
+					Update update = new Update(this, _message, assertStmt.getMessage());
+					_modifications.add(update);
+				}
+			} else if (_message.getBindingNode() != assertStmt.getMessage()) {
+				Update update = new Update(this, _message, assertStmt.getMessage());
+				_modifications.add(update);
+			} else {
+				_message.genModifications();
+			}
 			return true;
 		}
 		return false;
 	}
 
 	@Override
-	public boolean genModifications() {
-		return true;
-	}
-
-	@Override
 	public boolean ifMatch(Node node, Map<Node, Node> matchedNode, Map<String, String> matchedStrings, MatchLevel level) {
 		if(node instanceof AssertStmt) {
-			return super.ifMatch(node, matchedNode, matchedStrings, level);
+			AssertStmt assertStmt = (AssertStmt) node;
+			return getExpression().ifMatch(assertStmt.getExpression(), matchedNode, matchedStrings, level)
+					|| super.ifMatch(node, matchedNode, matchedStrings, level);
 		}
 		return false;
 	}
@@ -122,7 +207,16 @@ public class AssertStmt extends Stmt {
 	public StringBuffer transfer(VarScope vars, Map<String, String> exprMap, String retType, Set<String> exceptions) {
 		StringBuffer stringBuffer = super.transfer(vars, exprMap, retType, exceptions);
 		if (stringBuffer == null) {
-			stringBuffer = toSrcString();
+			stringBuffer = new StringBuffer("assert ");
+			StringBuffer tmp = _expresion.transfer(vars, exprMap, retType, exceptions);
+			if (tmp == null) return null;
+			stringBuffer.append(tmp);
+			if (_message != null) {
+				tmp = _message.transfer(vars, exprMap, retType, exceptions);
+				if (tmp == null) return null;
+				stringBuffer.append(":").append(tmp);
+			}
+			stringBuffer.append(";");
 		}
 		return stringBuffer;
 	}
@@ -130,6 +224,48 @@ public class AssertStmt extends Stmt {
 	@Override
 	public StringBuffer adaptModifications(VarScope vars, Map<String, String> exprMap, String retType,
                                            Set<String> exceptions) {
-		return toSrcString();
+		StringBuffer expression = null;
+		StringBuffer message = null;
+		Node pnode = NodeUtils.checkModification(this);
+		if (pnode != null) {
+			AssertStmt assertStmt = (AssertStmt) pnode;
+			for (Modification modification : assertStmt.getModifications()) {
+				if (modification instanceof Update) {
+					Update update = (Update) modification;
+					Node changedNode = update.getSrcNode();
+					if (changedNode == assertStmt.getExpression()) {
+						expression = update.apply(vars, exprMap, retType, exceptions);
+						if (expression == null) return null;
+					} else if (changedNode == assertStmt.getMessage()) {
+						message = update.apply(vars, exprMap, retType, exceptions);
+						if (message == null) return null;
+					}
+				} else {
+					LevelLogger.error("@AssertStmt Should not be this kind of modification : " + modification);
+				}
+			}
+		}
+
+		StringBuffer stringBuffer = new StringBuffer("assert ");
+		StringBuffer tmp;
+		if(expression == null) {
+			tmp = _expresion.adaptModifications(vars, exprMap, retType, exceptions);
+			if(tmp == null) return null;
+			stringBuffer.append(tmp);
+		} else {
+			stringBuffer.append(expression);
+		}
+
+		if (message == null) {
+			if (_message != null) {
+				tmp = _message.adaptModifications(vars, exprMap, retType, exceptions);
+				if (tmp == null) return null;
+				stringBuffer.append(":").append(tmp);
+			}
+		} else {
+			stringBuffer.append(":").append(message);
+		}
+		stringBuffer.append(";");
+		return stringBuffer;
 	}
 }
