102aa2c77a64e72cbfe5d0da4e37af8dc287e6d4
xgdsmileboy <jiajun.jiang@pku.edu.cn>
2019-04-25 15:46:46 -0500

    optimize matching

--- src/main/java/mfix/core/node/match/MatchList.java
+++ src/main/java/mfix/core/node/match/MatchList.java
@@ -30,9 +30,12 @@ public class MatchList {
 
     public MatchList setMatchedNodes(List<MatchNode> nodes) {
         _matchNodes = new ArrayList<>(nodes);
-        _matchNodes = _matchNodes.stream()
-                .sorted(Comparator.comparingDouble(MatchNode::getScore).reversed())
-                .limit(Constant.MAX_INSTANCE_PER_PATTERN).collect(Collectors.toList());
+        if (_pNode.getAllChildren().size() > 5) {
+            // complex expression can be filtered ahead of time
+            _matchNodes = _matchNodes.stream()
+                    .sorted(Comparator.comparingDouble(MatchNode::getScore).reversed())
+                    .limit(Constant.MAX_INSTANCE_PER_PATTERN).collect(Collectors.toList());
+        }
         return this;
     }
 
